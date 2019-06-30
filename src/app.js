1577f06648902a7dd46dcfb80129ddee6b6f2791
tanisha2398 <tanishanegipro@gmail.com>
2019-06-30 16:38:38 +0530

    add bubel loader to compile ES6 AND ES7 JavaScript to ES5

---
+++ src/app.js
@@ -0,0 +1,5 @@
+// let test = () => console.log("hi tani");
+// test();
+import { person, sayhi } from "./lib";
+console.log(person.name);
+console.log(sayhi("tan"));
---
+++ src/lib.js
@@ -0,0 +1,8 @@
+export const person = {
+  name: "tanisha",
+  location: "rawatbhata",
+  age: 21
+};
+export function sayhi(name) {
+  return `hello ${name}`;
+}
